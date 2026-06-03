f204ca951b8e48eb0f02e9ae60106fac1ecaf034
Max Reil <152213451+maxreil@users.noreply.github.com>
2026-06-03 09:21:50 -0700

    Update fetch_data.py

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -1,23 +1,25 @@
 import yfinance as yf
 
-def get_stock_data(ticker):
+def get_stock(ticker):
     stock = yf.Ticker(ticker)
-
-    return {
-        "cashflow": stock.cashflow,
-        "financials": stock.financials,
-        "balance_sheet": stock.balance_sheet
-    }
 
 def get_free_cash_flow(ticker):
-    stock = yf.Ticker(ticker)
+    stock = get_stock(ticker)
     cashflow = stock.cashflow
 
     try:
-        fcf = cashflow.loc["Free Cash Flow"]
-        latest_fcf = fcf.iloc[0]
-        return latest_fcf
-
-    except Exception as e:
-        print(f"Error extracting FCF: {e}")
+        fcf_series = cashflow.loc["Free Cash Flow"]
+        return fcf_series.iloc[0]
+    except Exception:
         return None
+
+
+def get_market_price(ticker):
+    stock = get_stock(ticker)
+
+    try:
+        price = stock.history(period="1d")["Close"].iloc[-1]
+        return price
+    except Exception:
+        return None
+                              
